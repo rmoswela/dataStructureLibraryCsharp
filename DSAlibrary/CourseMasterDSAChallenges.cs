6005ec8a809c01b09f90a27d68aaac8c752126e4
Reuben <rmoswela@gmail.com>
2022-07-30 01:54:09 +0200

    adds a function to shuffle array

--- DSAlibrary/CourseMasterDSAChallenges.cs
+++ DSAlibrary/CourseMasterDSAChallenges.cs
@@ -237,5 +237,30 @@ namespace DataStructures
 
       return strs[0];
     }
+
+    /*
+     * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+     * Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+     * Example1: Input: nums = [2,5,1,3,4,7], n = 3 Output: [2,3,5,4,1,7] 
+     * Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+     * Example2: Input: nums = [1,2,3,4,4,3,2,1], n = 4 Output: [1,4,2,3,3,2,4,1]
+     * Example3: Input: nums = [1,1,2,2], n = 2 Output: [1,2,1,2]
+     * Constraints: 1 <= n <= 500    nums.length == 2n    1 <= nums[i] <= 10^3
+     */
+    //Time Complexity and Space Complexity is (0)n
+    public int[] Shuffle(int[] nums, int n)
+    {
+      var arr = new int[nums.Length];
+      int index = 0, x = 0, y = n;
+      while (index < nums.Length) {
+        if (index % 2 == 0) {
+          arr[index] = nums[x++];
+        }
+        else {
+          arr[index] = nums[y++];
+          index++;
+        }
+        return arr;
+    }
   }
 }
