c46a3e78aff39db2bb1cd547dd9887f8b3d0c6c4
Reuben Moswela <Reuben.Moswela@derivco.co.za>
2022-08-05 17:04:51 +0200

    adds program to call the library for dev testing

---
+++ AppToRunLibray/App.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DataStructures;
+
+namespace AppToRunLibrary
+{
+  internal class App
+  {
+    private static void Main(string[] args)
+    {
+      var arr = new myArray();
+      Console.WriteLine("Array: {0}, Arr Length: {1}", arr, arr.Length);
+      arr.AddItem("Reuben");
+      arr.AddItem("Moswela");
+      arr.AddItem("Neo");
+      arr.AddItem("Khachana");
+      Console.WriteLine("Item: {0}, ArrayLength: {1}", arr.GetItem(3), arr.Length);
+    }
+  }
+}
