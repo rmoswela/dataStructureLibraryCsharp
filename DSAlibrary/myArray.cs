f4b838df88039a42e7022faa46c9f193fed37c5a
Reuben Moswela <Reuben.Moswela@derivco.co.za>
2022-08-05 17:42:14 +0200

    refactoring the code

--- DSAlibrary/myArray.cs
+++ DSAlibrary/myArray.cs
@@ -10,8 +10,8 @@ namespace DataStructures
     //array is empty and of length zero at instantiation
     public myArray()
     {
-      this._length = 0;
-      this._data = new dynamic[]{};
+      _length = 0;
+      _data = new dynamic[]{};
     }
 
     //get data at a specified index
@@ -27,34 +27,30 @@ namespace DataStructures
     {
       var count = 0;
       var newArray = new dynamic[_length + 1];
-      for (var loopIndex = 0; loopIndex < this._data.Length; loopIndex++) {
-        newArray[count++] = this._data[loopIndex];
+      for (var loopIndex = 0; loopIndex < _data.Length; loopIndex++) {
+        newArray[count++] = _data[loopIndex];
       }
       newArray[count] = t;
-      this._data = newArray;
-      this._length = _length + 1;
+      _data = newArray;
+      _length++;
     }
 
     //removes last item in the array
     //time complexity for Deletion of last item = 0(n) average and worst case
-    public int RemoveLastItem()
+    public void RemoveLastItem()
     {
       var newArray = new dynamic [_length - 1];
-      for (int loopIndex = 0, count = 0; loopIndex < this._length - 1; loopIndex++)
-      {
-        newArray[count++] = this._data[loopIndex];
+      for (int loopIndex = 0, count = 0; loopIndex < _length - 1; loopIndex++) {
+        newArray[count++] = _data[loopIndex];
       }
-
-      this._data = newArray;
-      _length = _length - 1;
-      return _length;
+      _data = newArray;
+      _length--;
     }
 
     //length property
     public int Length
     {
       get => _length;
-      set => _length = value;
     }
   }
 }
