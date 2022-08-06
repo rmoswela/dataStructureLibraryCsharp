cbea3079299db34cfc58f05d0dc9fd80d367a8f6
Reuben <rmoswela@gmail.com>
2022-08-06 23:58:57 +0200

    new function to add items

--- AppToRunLibray/App.cs
+++ AppToRunLibray/App.cs
@@ -9,16 +9,21 @@ namespace AppToRunLibrary
     {
       var arr = new myArray();
       Console.WriteLine("Array: {0}, Arr Length: {1}", arr, arr.Length);
-      arr.AddItem("Reuben");
-      arr.AddItem("Moswela");
-      arr.AddItem("Neo");
-      arr.AddItem("Khachana");
-      arr.AddItem("Meleko");
+      arr = AddItemsToArray(arr);
       Console.WriteLine("Item: {0}, ArrayLength: {1}", arr.GetItem(3), arr.Length);
       arr.RemoveLastItem();
       Console.WriteLine("ArrayLength after deletion: {0}, LastItem: {1}", arr.Length, arr.GetItem(arr.Length - 1));
       arr.RemoveItem(1);
       Console.WriteLine("ArrayLength after deletion of specified index: {0}, LastItem: {1}", arr.Length, arr.GetItem(1));
+    }
+
+    private static myArray AddItemsToArray(myArray inputArray)
+    {
+      var names = new string[] {"Reuben", "Moswela", "Neo", "Khachana", "Meleko", "Gabrielle" };
+      for (int loop = 0; loop < names.Length; loop++) {
+         inputArray.AddItem(names[loop]);
+      }
+      return inputArray;
     }
   }
 }
