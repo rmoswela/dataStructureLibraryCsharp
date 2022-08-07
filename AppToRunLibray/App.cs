38170675a6457d91035ae5a8e0f1ba9f170a227c
Reuben <rmoswela@gmail.com>
2022-08-07 21:38:51 +0200

    method to find item by value and return index

--- AppToRunLibray/App.cs
+++ AppToRunLibray/App.cs
@@ -3,27 +3,31 @@ using DataStructures;
 
 namespace AppToRunLibrary
 {
-  internal class App
-  {
-    private static void Main(string[] args)
+    internal class App
     {
-      var arr = new myArray();
-      Console.WriteLine("Array: {0}, Arr Length: {1}", arr, arr.Length);
-      arr = AddItemsToArray(arr);
-      Console.WriteLine("Item: {0}, ArrayLength: {1}", arr.GetItem(3), arr.Length);
-      arr.RemoveLastItem();
-      Console.WriteLine("ArrayLength after deletion: {0}, LastItem: {1}", arr.Length, arr.GetItem(arr.Length - 1));
-      arr.RemoveItem(1);
-      Console.WriteLine("ArrayLength after deletion of specified index: {0}, LastItem: {1}", arr.Length, arr.GetItem(1));
-    }
+        private static void Main(string[] args)
+        {
+            dynamic arr = new myArray();
+            Console.WriteLine("Array: {0}, Arr Length: {1}", arr, arr.Length);
+            arr = AddItemsToArray(arr);
+            Console.WriteLine("Item: {0}, ArrayLength: {1}", arr.GetItem(3), arr.Length);
+            arr.RemoveLastItem();
+            Console.WriteLine("ArrayLength after deletion: {0}, LastItem: {1}", arr.Length, arr.GetItem(arr.Length - 1));
+            arr.RemoveItem(1);
+            Console.WriteLine("ArrayLength after deletion of specified index: {0}, LastItem: {1}", arr.Length, arr.GetItem(1));
+            dynamic firstIndex = arr.FindIndexItemFirstOccurance("Reuben");
+            var lastIndex = arr.FindIndexItemLastOccurance("Reuben");
+            Console.WriteLine("ArrayLength: {0}, FirstOccuranceIndex: {1}, LastOccurenceIndex: {2}", arr.Length, firstIndex, lastIndex);
+        }
 
-    private static myArray AddItemsToArray(myArray inputArray)
-    {
-      var names = new string[] {"Reuben", "Moswela", "Neo", "Khachana", "Meleko", "Gabrielle" };
-      for (int loop = 0; loop < names.Length; loop++) {
-         inputArray.AddItem(names[loop]);
-      }
-      return inputArray;
+        private static myArray AddItemsToArray(myArray inputArray)
+        {
+            var names = new string[] { "Reuben", "Moswela", "Neo", "Khachana", "Meleko", "Reuben", "Gabrielle" };
+            for (int loop = 0; loop < names.Length; loop++)
+            {
+                inputArray.AddItem(names[loop]);
+            }
+            return inputArray;
+        }
     }
-  }
 }
--- DSAlibrary/myArray.cs
+++ DSAlibrary/myArray.cs
@@ -2,69 +2,94 @@
 
 namespace DataStructures
 {
-  public class myArray
-  {
-    private int _length;
-    private dynamic[] _data;
-
-    //array is empty and of length zero at instantiation
-    public myArray()
+    public class myArray
     {
-      _length = 0;
-      _data = new dynamic[]{};
-    }
+        private int _length;
+        private dynamic[] _data;
 
-    //get data at a specified index
-    //time complexity to Access = O(1) on average and worst case
-    public dynamic GetItem(int index)
-    {
-      return _data[index];
-    }
-
-    //add data at the end of the array
-    //time complexity for Insertion = O(n) on average and worst case
-    public void AddItem<T>(T t)
-    {
-      var count = 0;
-      var newArray = new dynamic[_length + 1];
-      for (var loopIndex = 0; loopIndex < _data.Length; loopIndex++) {
-        newArray[count++] = _data[loopIndex];
-      }
-      newArray[count] = t;
-      _data = newArray;
-      _length++;
-    }
-
-    //removes item at the specified index
-    //time complexity for this Deletion = 0(n) for both average and worst cases
-    public void RemoveItem(int index)
-    {
-      var newArray = new dynamic[_length - 1];
-      for (int loopIndex = 0, count = 0; loopIndex < _length; loopIndex++) {
-        if (loopIndex != index) {
-          newArray[count++] = _data[loopIndex];
+        //array is empty and of length zero at instantiation
+        public myArray()
+        {
+            _length = 0;
+            _data = new dynamic[] { };
         }
-      }
-      _data = newArray;
-      _length--;
-    }
 
-    //removes last item in the array
-    //time complexity for Deletion of last item = 0(n) average and worst case
-    public void RemoveLastItem()
-    {
-      var newArray = new dynamic [_length - 1];
-      for (int loopIndex = 0, count = 0; loopIndex < _length - 1; loopIndex++) {
-        newArray[count++] = _data[loopIndex];
-      }
-      _data = newArray;
-      _length--;
-    }
+        //get data at a specified index
+        //time complexity to Access = O(1) on average and worst case
+        public dynamic GetItem(int index)
+        {
+            return _data[index];
+        }
 
-    //length property
-    public int Length
-    {
-      get => _length;
+        //add data at the end of the array
+        //time complexity for Insertion = O(n) on average and worst case
+        public void AddItem<T>(T t)
+        {
+            var count = 0;
+            var newArray = new dynamic[_length + 1];
+            for (var loopIndex = 0; loopIndex < _data.Length; loopIndex++)
+            {
+                newArray[count++] = _data[loopIndex];
+            }
+            newArray[count] = t;
+            _data = newArray;
+            _length++;
+        }
+
+        //removes item at the specified index
+        //time complexity for this Deletion = 0(n) for both average and worst cases
+        public void RemoveItem(int index)
+        {
+            var newArray = new dynamic[_length - 1];
+            for (int loopIndex = 0, count = 0; loopIndex < _length; loopIndex++)
+            {
+                if (loopIndex != index)
+                {
+                    newArray[count++] = _data[loopIndex];
+                }
+            }
+            _data = newArray;
+            _length--;
+        }
+
+        //removes last item in the array
+        //time complexity for Deletion of last item = 0(n) average and worst case
+        public void RemoveLastItem()
+        {
+            var newArray = new dynamic[_length - 1];
+            for (int loopIndex = 0, count = 0; loopIndex < _length - 1; loopIndex++)
+            {
+                newArray[count++] = _data[loopIndex];
+            }
+            _data = newArray;
+            _length--;
+        }
+
+
+        //finds an index where the item first occurs
+        //time complexity for Search = 0(n) average and worst case
+        public int FindIndexItemFirstOccurance<T>(T t){
+            for (var loop = 0; loop < _length; loop++){
+                if (t == _data[loop])
+                return loop;
+            }
+            return -1;
+        }
+
+        //finds and returns index of items' last occurance
+        //time complexity for Search = 0(n) average and worst case
+        public int FindIndexItemLastOccurance<T>(T t){
+            for (int loop = _length - 1; loop >= 0; loop--){
+                if (t == _data[loop])
+                return loop;
+            }
+            return -1;
+        }
+
+        //length property
+        public int Length
+        {
+            get => _length;
+        }
     }
-  }
 }
