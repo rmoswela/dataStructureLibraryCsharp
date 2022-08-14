72af0b8776ae6c042204c03e98b8c26dea6de0a7
Reuben <rmoswela@gmail.com>
2022-08-14 20:20:26 +0200

    reduces the comments for last function

--- DSAlibrary/CourseMasterDSAChallenges.cs
+++ DSAlibrary/CourseMasterDSAChallenges.cs
@@ -302,13 +302,7 @@ namespace DataStructures
      * --X: X is decremented by 1, X =  0 - 1 = -1.
      * X++: X is incremented by 1, X = -1 + 1 =  0.
      * X++: X is incremented by 1, X =  0 + 1 =  1.
-     * Example2: Input: operations = ["++X","++X","X++"] Output: 3
-     * Explanation: The operations are performed as follows:
-     * Initially, X = 0.
-     * ++X: X is incremented by 1, X = 0 + 1 = 1.
-     * ++X: X is incremented by 1, X = 1 + 1 = 2.
-     * X++: X is incremented by 1, X = 2 + 1 = 3.
-     * Example3: Input: operations = ["X++","++X","--X","X--"] Output: 0
+     * Example2: Input: operations = ["X++","++X","--X","X--"] Output: 0
      * Explanation: The operations are performed as follows: Initially, X = 0.
      * X++: X is incremented by 1, X = 0 + 1 = 1.
      * ++X: X is incremented by 1, X = 1 + 1 = 2.
