3e1a3576505f04f15edb90a03f2df42942cec083
Reuben Moswela <rmoswela@gmail.com>
2022-09-14 21:01:10 +0200

    refactored tests

--- DataStructuresTesting/Array/AddItemTests.cs
+++ DataStructuresTesting/Array/AddItemTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System;
 using DataStructures;
 
 namespace DataStructuresTesting.Array
@@ -9,6 +8,16 @@ namespace DataStructuresTesting.Array
   {
     private MyArray _myArray;
 
+    private static object[] _validValues =
+    {
+      new [] {"Reuben", "Moswela"},
+      new [] {1, 2},
+      new [] {2.5, 9.0},
+      new [] {true, false},
+      new [] {'a', 'b', 'c'},
+      new [] {new Person("Reuben", Gender.Male, 100), new Person("Neo", Gender.Female, 5) }
+    };
+
     [SetUp]
     public void Setup()
     {
@@ -16,27 +25,19 @@ namespace DataStructuresTesting.Array
     }
 
     [Test]
-    [TestCase(typeof(object))]
-    [TestCase("Reuben")]//string
-    [TestCase(1)] //int
-    [TestCase(2.3)] //double
-    [TestCase('a')] //char
-    [TestCase(false)] //boolean
+    [TestCaseSource(nameof(_validValues))]
     public void AddItem_ValidValue_NumberOfItemsIncreaseByOne<T>(T value)
     {
-      // Arrange
-      Setup();
       // Act
       _myArray.AddItem(value);
-      var length = _myArray.Length;
       // Assert
       Assert.AreEqual(_myArray.Length, 1);
     }
+
     [Test]
     public void AddItem_NullValue_NumberOfItemsIncreaseByOne()
     {
       // Arrange
-      Setup();
       string value = null;
       //Act
       _myArray.AddItem(value);
@@ -48,42 +49,11 @@ namespace DataStructuresTesting.Array
     public void AddItem_EmptyItem_NumberOfItemsIncreaseByOne()
     {
       // Arrange
-      Setup();
       string value = string.Empty;
       //Act
       _myArray.AddItem(value);
       // Assert
       Assert.AreEqual(_myArray.Length, 1);
-    }
-
-    [Test]
-    public void AddItem_MultipleValues_ReturnEqualNumberOfItemsAdded()
-    {
-      // Arrange
-      Setup();
-      string value1 = "Reuben", value2 = null, value3 = "Moswela", value4 = "Lame", value5 = ".";
-      //Act
-      _myArray.AddItem(value1);
-      _myArray.AddItem(value2);
-      _myArray.AddItem(value3);
-      _myArray.AddItem(value4);
-      _myArray.AddItem(value5);
-      // Assert
-      Assert.AreEqual(_myArray.Length, 5);
-    }
-
-    //[Test]
-    public void AddItem_AddItemOfDifferentDataType_NumberOfItemsIncreaseByOne()
-    {
-      // Arrange
-      Setup();
-      string value = "Reuben";
-      int wrongDataTypeValue = 1;
-      //Act
-      _myArray.AddItem(value);
-      //_myArray.AddItem(wrongDataTypeValue);
-      // Assert
-      Assert.Throws<ArrayTypeMismatchException>(()=>_myArray.AddItem(wrongDataTypeValue));
     }
   }
 }
---
+++ DataStructuresTesting/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace DataStructuresTesting
+{
+  /*
+   * An object helper class for when performing tests
+   */
+
+  internal enum Gender
+  {
+    Male,
+    Female
+  }
+  internal class Person
+  {
+    private string _name;
+    private int _age;
+    private Gender _gender;
+
+    public Person(string name, Gender gender, int age)
+    {
+      _name = name;
+      _gender = gender;
+      _age = age;
+    }
+  }
+}
