b42631497f678fdf81c48a37d6a40c0059ce2b00
Reuben Moswela <rmoswela@gmail.com>
2022-09-20 13:52:25 +0200

    adds the array reverse tests

---
+++ DataStructuresTesting/Array/ReverseTests.cs
@@ -0,0 +1,42 @@
+﻿using DataStructures;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace DataStructuresTesting.Array
+{
+  [TestFixture]
+  public class ReverseTests
+  {
+    private MyArray _myArray;
+    private static readonly TestData TestData = new TestData();
+    private readonly List<dynamic> _list = TestData.InitializationValues;
+
+    [SetUp]
+    public void Setup()
+    {
+      _myArray = new MyArray(_list);
+    }
+
+    [Test]
+    public void Reverse_ArrayWithValidElements_AllElementsShouldBeReversed()
+    {
+      //Arrange
+      MyArray myArrayBeforeReverse = new MyArray();
+      myArrayBeforeReverse.Copy(_myArray, _myArray.Length);
+      //Act
+      _myArray.Reverse();
+      //Assert
+      Assert.AreNotEqual(_myArray, myArrayBeforeReverse);
+    }
+
+    [Test]
+    public void Reverse_NullArray_ThrowsNullReferenceException()
+    {
+      //Arrange
+      _myArray = null;
+      //Act and Assert
+      Assert.Throws<NullReferenceException>(() => _myArray.Reverse());
+    }
+  }
+}
