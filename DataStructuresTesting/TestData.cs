b98ac24dc41f271894a1b1f560d33062468b5762
Reuben Moswela <rmoswela@gmail.com>
2022-09-22 20:22:16 +0200

    adds test data

---
+++ DataStructuresTesting/TestData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructuresTesting
+{
+  internal class TestData
+  {
+    public object[] ValidValues { get; } =
+    {
+      new [] {"Reuben", "Moswela"},
+      new [] {1, 2},
+      new [] {2.5, 9.0},
+      new [] {true, false},
+      new [] {'a', 'b', 'c'},
+      new [] {new Person("Reuben", Gender.Male, 100), new Person("Neo", Gender.Female, 5) }
+    };
+
+    public List<dynamic> InitializationValues { get; } = new List<dynamic>() { "r", "e", "u", "b", "e" };
+  }
+}
