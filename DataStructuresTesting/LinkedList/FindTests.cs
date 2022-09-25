18727d566aa7ec0290e90d74f389bbb438861965
Reuben Moswela <rmoswela@gmail.com>
2022-09-25 23:18:06 +0200

    adds tests to the find an element method

---
+++ DataStructuresTesting/LinkedList/FindTests.cs
@@ -0,0 +1,25 @@
+﻿using DataStructures;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace DataStructuresTesting.LinkedList
+{
+  [TestFixture]
+  public class FindTests
+  {
+    private readonly TestData TestData = new TestData();
+
+    [Test]
+    public void Find_validValue_ReturnsTheValue()
+    {
+      //Arrange
+      ICollection<string> names = new[] {"Reuben", "Neo", "Lame", "Moswela"};
+      MyLinkedList<string> myLinkedList = new MyLinkedList<string>(names);
+      //Act
+      string results = myLinkedList.Find(x => x.Contains("Lame"));
+      //Assert
+      Assert.AreEqual("Lame",  results);
+    }
+  }
+}
