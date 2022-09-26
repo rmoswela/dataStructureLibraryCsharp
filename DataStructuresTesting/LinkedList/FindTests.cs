28a55d53693f4cfcde0998e863f595156285f3a1
Reuben Moswela <rmoswela@gmail.com>
2022-09-26 18:21:48 +0200

    adds test to find element that is not available in the linkedlist

--- DataStructuresTesting/LinkedList/FindTests.cs
+++ DataStructuresTesting/LinkedList/FindTests.cs
@@ -8,18 +8,31 @@ namespace DataStructuresTesting.LinkedList
   [TestFixture]
   public class FindTests
   {
-    private readonly TestData TestData = new TestData();
+    private static readonly TestData TestData = new TestData();
+    private static readonly IEnumerable<string> TestValues = TestData.EnumerableTestValues;
 
     [Test]
-    public void Find_validValue_ReturnsTheValue()
+    [TestCase("Lame")]
+    public void Find_ValidElementThatMatchesConditionDefinedByPredicate_ReturnsFirstOccurrenceOfTheElement(string value)
     {
       //Arrange
-      ICollection<string> names = new[] {"Reuben", "Neo", "Lame", "Moswela"};
-      MyLinkedList<string> myLinkedList = new MyLinkedList<string>(names);
+      MyLinkedList<string> myLinkedList = new MyLinkedList<string>(TestValues);
       //Act
-      string results = myLinkedList.Find(x => x.Contains("Lame"));
+      string results = myLinkedList.Find(x => x.Contains(value));
       //Assert
-      Assert.AreEqual("Lame",  results);
+      Assert.AreEqual(value,  results);
+    }
+
+    [Test]
+    [TestCase("SeeSharp")]
+    public void Find_AnElementThatDoesNotMatchesConditionDefinedByPredicate_ReturnDefaultValue(string value)
+    {
+      //Arrange
+      MyLinkedList<string> myLinkedList = new MyLinkedList<string>(TestValues);
+      //Act
+      string results = myLinkedList.Find(x => x.Contains(value));
+      //Assert
+      Assert.AreEqual(value, results);
     }
   }
 }
