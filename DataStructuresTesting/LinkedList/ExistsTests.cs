b0d02010458f3bf7a5900968a2c6cc41d5f81552
Reuben Moswela <rmoswela@gmail.com>
2022-10-16 01:06:35 +0200

    adds test for exists method when predicate does not match any elements

--- DataStructuresTesting/LinkedList/ExistsTests.cs
+++ DataStructuresTesting/LinkedList/ExistsTests.cs
@@ -18,5 +18,17 @@ namespace DataStructuresTesting.LinkedList
       //Act and Assert
       Assert.Throws<ArgumentNullException>(() => myLinkedList.Exists(predicate));
     }
+
+    [Test]
+    public void Exists_ConditionThatDoesNotMatchAnyElement_ReturnFalse()
+    {
+      //Arrange
+      Predicate<double> predicate = x => x % 3 == 0;
+      MyLinkedList<double> myLinkedList = new MyLinkedList<double>(TestData.squaredDoubles);
+      //Act
+      var result = myLinkedList.Exists(predicate);
+      //Assert
+      Assert.IsFalse(result);
+    }
   }
 }
