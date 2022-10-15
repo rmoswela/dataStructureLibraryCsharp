d440b98ab8f2d8174b1a4df75759e2f2fffc002e
Reuben Moswela <rmoswela@gmail.com>
2022-10-16 00:49:49 +0200

    adds test for exists method when predicate null

---
+++ DataStructuresTesting/LinkedList/ExistsTests.cs
@@ -0,0 +1,22 @@
+﻿using DataStructures;
+using NUnit.Framework;
+using System;
+
+namespace DataStructuresTesting.LinkedList
+{
+  [TestFixture]
+  public class ExistsTests
+  {
+    private static readonly TestData TestData = new TestData();
+
+    [Test]
+    [TestCase(null)]
+    public void Exists_ConditionOfANullPredicate_ThrowsArgumentNullException(Predicate<string> predicate)
+    {
+      //Arrange
+      MyLinkedList<string> myLinkedList = new MyLinkedList<string>(TestData.EnumerableTestValues);
+      //Act and Assert
+      Assert.Throws<ArgumentNullException>(() => myLinkedList.Exists(predicate));
+    }
+  }
+}
