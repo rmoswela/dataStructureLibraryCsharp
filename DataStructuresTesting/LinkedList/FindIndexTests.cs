4652a12aa2b450a1ecfaf52727c07982ba76827e
Reuben Moswela <rmoswela@gmail.com>
2022-10-15 21:31:02 +0200

    adds find index of null predicate test

---
+++ DataStructuresTesting/LinkedList/FindIndexTests.cs
@@ -0,0 +1,24 @@
+﻿using DataStructures;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace DataStructuresTesting.LinkedList
+{
+  [TestFixture]
+  public class FindIndexTests
+  {
+    private static readonly TestData TestData = new TestData();
+    private static readonly IEnumerable<string> TestValues = TestData.EnumerableTestValues;
+
+    [Test]
+    [TestCase(null)]
+    public void FindIndex_OfNullPredicate_ThrowsArgumentNullException(Predicate<string> predicate)
+    {
+      //Arrange
+      MyLinkedList<string> myLinkedList = new MyLinkedList<string>(TestValues);
+      //Act and Assert
+      Assert.Throws<ArgumentNullException>(() => myLinkedList.FindIndex(predicate));
+    }
+  }
+}
