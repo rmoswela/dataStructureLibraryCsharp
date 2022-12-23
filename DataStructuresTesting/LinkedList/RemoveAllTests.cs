19909e6e9342bd88d7db31f6909aa16b58d3bbc3
Reuben Moswela <rmoswela@gmail.com>
2022-12-23 22:00:42 +0200

    adds test to remove all prime number values less or equal to 5

---
+++ DataStructuresTesting/LinkedList/RemoveAllTests.cs
@@ -0,0 +1,24 @@
+﻿using DataStructures;
+using NUnit.Framework;
+
+namespace DataStructuresTesting.LinkedList
+{
+  [TestFixture]
+  public class RemoveAllTests
+  {
+    private static readonly TestData TestData = new TestData();
+
+    [Test]
+    public void RemoveAll_NumbersLessThanFiveInPrimeNumberList_ReturnsThreeAsNumberOfElementRemoved()
+    {
+      //Arrange
+      MyLinkedList<int> myLinkedList = new MyLinkedList<int>(TestData.primeNumbers);
+
+      //Act
+      var results = myLinkedList.RemoveAll(value => value <= 5);
+
+      //Assert
+      Assert.That(results, Is.EqualTo(3));
+    }
+  }
+}
