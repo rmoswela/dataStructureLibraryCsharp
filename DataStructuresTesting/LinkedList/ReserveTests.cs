e7b5be0f5f56d5bf50d93f155f2026cff7870889
Reuben Moswela <rmoswela@gmail.com>
2022-12-23 18:44:38 +0200

    adds empty list reverse test

--- DataStructuresTesting/LinkedList/ReserveTests.cs
+++ DataStructuresTesting/LinkedList/ReserveTests.cs
@@ -1,5 +1,6 @@
 ﻿using DataStructures;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace DataStructuresTesting.LinkedList
 {
@@ -13,14 +14,27 @@ namespace DataStructuresTesting.LinkedList
     {
       //Arrange
       MyLinkedList<int> myLinkedList = new MyLinkedList<int>(TestData.primeNumbers);
-      var expected = new[] { 23, 19, 17, 7, 5, 3, 2 };
+      var actual = new[] { 23, 19, 17, 7, 5, 3, 2 };
       //Act
       myLinkedList.Reverse();
       //Assert
-      Assert.AreEqual(expected[0], myLinkedList[0]);
-      Assert.AreEqual(expected[3], myLinkedList[3]);
-      Assert.AreEqual(expected[6], myLinkedList[6]);
+      //Assert.That(myLinkedList, Is.EquivalentTo(actual));
+      Assert.AreEqual(actual[0], myLinkedList[0]);
+      Assert.AreEqual(actual[3], myLinkedList[3]);
+      Assert.AreEqual(actual[6], myLinkedList[6]);
 
+    }
+
+    [Test]
+    public void Reverse_AnEmptyList_ReturnsEmptyList()
+    {
+      //Arrange
+      MyLinkedList<int> myLinkedList = new MyLinkedList<int>();
+      var listCountBefore = myLinkedList.Count;
+      //Act
+      myLinkedList.Reverse();
+      //Assert
+      Assert.That(listCountBefore, Is.EqualTo(myLinkedList.Count));
     }
   }
 }
