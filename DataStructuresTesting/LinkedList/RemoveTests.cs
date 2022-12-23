baa52b8e2a253b55b2133446e24e388141760f7f
Reuben Moswela <rmoswela@gmail.com>
2022-12-23 19:39:29 +0200

    adds test to remove valid string

---
+++ DataStructuresTesting/LinkedList/RemoveTests.cs
@@ -0,0 +1,23 @@
+﻿using DataStructures;
+using NUnit.Framework;
+
+namespace DataStructuresTesting.LinkedList
+{
+  [TestFixture]
+  public class RemoveTests
+  {
+    private static readonly TestData TestData = new TestData();
+
+    [Test]
+    [TestCase("Moswela")]
+    public void Remove_ValidString_ReturnsTrue(string value)
+    {
+      //Arrange
+      MyLinkedList<string> myLinkedList = new MyLinkedList<string>(TestData.EnumerableTestValues);
+      //Act
+      var results = myLinkedList.Remove(value);
+      //Assert
+      Assert.IsTrue(results);
+    }
+  }
+}
