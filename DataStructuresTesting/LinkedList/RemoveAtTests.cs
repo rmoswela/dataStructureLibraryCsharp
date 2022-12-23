52aa6c1a4625d07f84d9c462625b5f438da06b41
Reuben Moswela <rmoswela@gmail.com>
2022-12-23 21:30:19 +0200

    adds test to remove at first index

---
+++ DataStructuresTesting/LinkedList/RemoveAtTests.cs
@@ -0,0 +1,26 @@
+﻿using DataStructures;
+using NUnit.Framework;
+
+namespace DataStructuresTesting.LinkedList
+{
+  [TestFixture]
+  public class RemoveAtTests
+  {
+    private static readonly TestData TestData = new TestData();
+
+    [Test]
+    [TestCase(0)]
+    public void RemoveAt_FirstIndexOfValidList_ReturnsListCountReducedByOne(int value)
+    {
+      //Arrange
+      MyLinkedList<int> myLinkedList = new MyLinkedList<int>(TestData.primeNumbers);
+      var listCountBefore = myLinkedList.Count;
+      var valueToRemove = myLinkedList[value];
+      //Act
+      myLinkedList.RemoveAt(value);
+      //Assert
+      Assert.That(myLinkedList.Count, Is.LessThan(listCountBefore));
+      Assert.AreNotEqual(valueToRemove, myLinkedList[value]);
+    }
+  }
+}
